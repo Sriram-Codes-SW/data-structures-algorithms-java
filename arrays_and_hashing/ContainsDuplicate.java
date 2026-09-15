c4e48f2cbc781448f6fdf9b327e5d7f285dccb0b
Sriram-Codes-SW <329133251+Sriram-Codes-SW@users.noreply.github.com>
2026-09-15 16:36:44 +0530

    Optimized for Time solution

--- arrays_and_hashing/ContainsDuplicate.java
+++ arrays_and_hashing/ContainsDuplicate.java
@@ -4,20 +4,20 @@ package arrays_and_hashing;
  * LeetCode 217 - Contains Duplicate
  * https://leetcode.com/problems/contains-duplicate/
  *
- * Approach: Two passes over the data using loops. If the second loop finds it, it is a duplicate.
+ * Approach: Space Time TradeOff using HashSet for O(1) insertion and access
+ *           and checking HashSet add() return value
  *
- * Time Complexity: O(n^2) - two passes, O(n) per pass
- * Space Complexity: O(1) - No extra data structures were used
+ * Time Complexity: O(n) - Single Pass
+ * Space Complexity: O(n) - No extra data structures were used
  */
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        for(int i = 0; i < nums.length;i++) {
-         int temp= nums[i];
-         for(int j= 0; j < nums.length;j++){
-             if(j != i && nums[j] == temp){
-                 return true;
-             }
-         }
+        Set<Integer> notDuplicate= new HashSet<Integer>();
+        for(int i= 0; i < nums.length;i++){
+            //Check the add method return value to determine duplicate
+            if(!notDuplicate.add(nums[i])){
+                return true;
+            }
         }
         return false;
     }
